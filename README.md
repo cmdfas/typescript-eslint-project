5000ae3e20237c81e7a995c49e747b3a3afa91cf
cmdfas <cmdfas@gmail.com>
2021-09-14 10:06:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# 如何在TypeScript项目中使用ESLint?
+
+
+## 运行Lint
+
+```bash
+npm run lint
+# 自动修复
+npm run lint:fix
+```
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('my typescript eslint project');
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
